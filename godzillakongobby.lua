c1de460ec9c1001b7081b366db4bcc2a00f93f32
Bardium <114866826+bardium@users.noreply.github.com>
2024-04-07 13:56:39 -0400

    feat: add rejoin on taking too long

--- godzillakongobby.lua
+++ godzillakongobby.lua
@@ -17,23 +17,74 @@ local HttpService = game:GetService("HttpService")
 
 local localPlayer = Players.LocalPlayer
 local itemPickups = Workspace:WaitForChild("Temp"):WaitForChild("ItemPickups")
-local cube = Workspace:WaitForChild("CrystalLobby"):WaitForChild("OpeningCutscene"):WaitForChild("ArmCamera"):WaitForChild("Cube")
+local cube = Workspace:WaitForChild("CrystalLobby")
+	:WaitForChild("OpeningCutscene")
+	:WaitForChild("ArmCamera")
+	:WaitForChild("Cube")
 
-local httprequest = (syn and syn.request) or (http and http.request) or http_request or (fluxus and fluxus.request) or request
+local httprequest = (syn and syn.request)
+	or (http and http.request)
+	or http_request
+	or (fluxus and fluxus.request)
+	or request
 local queueonteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
 
 local teleporting = false
 
-repeat task.wait() until localPlayer.Character ~= nil
+repeat
+	task.wait()
+until localPlayer.Character ~= nil
 
 local rootPart = localPlayer.Character:WaitForChild("HumanoidRootPart")
 
 rootPart.CFrame = cube.CFrame
 
-repeat task.wait() until Workspace.CurrentCamera.CameraType == Enum.CameraType.Scriptable
-repeat task.wait() until Workspace.CurrentCamera.CameraType ~= Enum.CameraType.Scriptable
+repeat
+	task.wait()
+until Workspace.CurrentCamera.CameraType == Enum.CameraType.Scriptable
+repeat
+	task.wait()
+until Workspace.CurrentCamera.CameraType ~= Enum.CameraType.Scriptable
 
 task.wait(1)
+
+local currencyLabel = localPlayer.PlayerGui.CurrencyUI.Container.CurrencyLabel
+
+local oldAmount = 0
+local timesFailed = 0
+
+local function rejoin()
+	if queueonteleport then
+		--queueonteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/bardium/random/main/godzillakongobby.lua'))()")
+	end
+	if httprequest and not teleporting then
+		local servers = {}
+		local req = httprequest({
+			Url = string.format(
+				"https://games.roblox.com/v1/games/%d/servers/Public?sortOrder=Desc&limit=100&excludeFullGames=true",
+				placeId
+			),
+		})
+		local body = HttpService:JSONDecode(req.Body)
+		if body and body.data then
+			for i, v in next, body.data do
+				if
+					type(v) == "table"
+					and tonumber(v.playing)
+					and tonumber(v.maxPlayers)
+					and v.playing < v.maxPlayers
+					and v.id ~= jobId
+				then
+					table.insert(servers, 1, v.id)
+				end
+			end
+		end
+		if #servers > 0 then
+			teleporting = true
+			TeleportService:TeleportToPlaceInstance(placeId, servers[math.random(1, #servers)], localPlayer)
+		end
+	end
+end
 
 while task.wait() do
 	local pickups = {}
@@ -47,30 +98,20 @@ while task.wait() do
 		local distanceFromCharacterB = (rootPart.Position - b:GetPivot().Position).Magnitude
 		return distanceFromCharacterA < distanceFromCharacterB
 	end)
-	if #pickups > 0 then
+	if #pickups > 10 then
 		for _, pickup in pickups do
-			rootPart.CFrame = pickup:GetPivot()
-			task.wait()
+			local attempts = 0
+			repeat
+				task.wait(0.1)
+				attempts += 1
+				rootPart.CFrame = pickup:GetPivot()
+			until tonumber(currencyLabel.Text) > oldAmount or attempts > 10
+			if attempts > 10 then
+				rejoin()
+			end
+			oldAmount = tonumber(currencyLabel.Text)
 		end
 	else
-		if queueonteleport then
-			--queueonteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/bardium/random/main/godzillakongobby.lua'))()")
-		end
-		if httprequest and not teleporting then
-			local servers = {}
-			local req = httprequest({Url = string.format("https://games.roblox.com/v1/games/%d/servers/Public?sortOrder=Desc&limit=100&excludeFullGames=true", placeId)})
-			local body = HttpService:JSONDecode(req.Body)
-			if body and body.data then
-				for i, v in next, body.data do
-					if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing < v.maxPlayers and v.id ~= jobId then
-						table.insert(servers, 1, v.id)
-					end
-				end
-			end
-			if #servers > 0 then
-				teleporting = true
-				TeleportService:TeleportToPlaceInstance(placeId, servers[math.random(1, #servers)], localPlayer)
-			end
-		end
+		rejoin()
 	end
 end
